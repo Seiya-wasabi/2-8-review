ee1a6c41b258264b9f86620aaf342406d1ea2da6
EC2 Default User <ec2-user@ip-172-31-2-165.ec2.internal>
2021-03-14 03:45:01 +0000

    演習1完了

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -3,7 +3,6 @@ puts "何回繰り返しますか"
 number = gets.to_i
 for number 1..9 do
   
-  
 puts "2回目の計算"
 puts "2つの値を入力してください"
 a = gets
---
+++ practice.rb
@@ -0,0 +1,11 @@
+for number in 1..6 do
+  puts number
+end
+
+puts "二つの値を入力しなさい"
+input1 = gets.to_i
+input2 = gets.to_i
+
+puts "#{input1*input2}"
+
+puts "結果です"
