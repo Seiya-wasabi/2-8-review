ac4c8a47b066beadd740fc0a31f3c0ad7b33a8f7
EC2 Default User <ec2-user@ip-172-31-2-165.ec2.internal>
2021-03-16 03:39:00 +0000

    デフォルト値

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -1,10 +1,7 @@
-def ask
-  p "質問はYouTubeコメント欄へ"
-end
-
-def say_age(me,age)
+def say_age(me="私",age=30)
   p "#{me}は#{age}才です"
 end
 
-ask
-say_age("佐藤誠也",24)
+say_age(nil,20)
+say_age
+say_age("君",15)
