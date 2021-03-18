5c2e66d67cfbc2b111d0b02857d87c38c35baa4f
EC2 Default User <ec2-user@ip-172-31-2-165.ec2.internal>
2021-03-18 02:45:42 +0000

    finish

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -5,20 +5,24 @@
 # 3と5の両方で割り切れる数値を引数に渡すと、「FizzBuzz」を返す
 # それ以外の数値は、その数値を文字列に変えて返す
 
-def number (input)
-  return "結果は..#{input}です！"
-
-puts "数字を入力してください"
-a = gets.to_i
-
-if a % 3 == 0
+def number(input)
+if input % 3 == 0
   "Fizz"
   
-elsif a % 5 == 0
+elsif input % 5 == 0
   "Buzz"
   
-elsif a % 3 == 0 && a % 5 == 0
+elsif input % 3 == 0 && a % 5 == 0
   "FizzBuzz"
   
+else
+  input.to_s
+  
+  end
 end
-end
+  
+  puts "数字を入力してください"
+  a = gets.to_i
+  
+  puts "結果は・・・"
+  puts number(a)
