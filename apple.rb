bcd5b34d559dee400c249b880fd879fac9d1d9ee
EC2 Default User <ec2-user@ip-172-31-2-165.ec2.internal>
2021-04-02 01:11:57 +0000

    [start]2-6

--- apple.rb
+++ apple.rb
@@ -1,8 +1,10 @@
-apple = "Yamanashi"
-if apple =="Aomori"
-  puts "このリンゴは青森県産です"
+apple = "Nagano"
 
-elsif apple == "Nagano"
-  puts "このリンゴは青森県産ではなく長野産です"
-else
-  puts "このリンゴは長野県産でも青森県産でもありません"
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+  elsif apple == "Nagano"
+    puts "このリンゴは青森県産ではなく、長野県産です"
+  else 
+    puts "このリンゴは青森県産でも長野県産でもありません。"
+  end
+    
